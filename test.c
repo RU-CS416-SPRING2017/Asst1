c9e89e10ae5932fb91d6ff399cb611648309a0b9
Ammaar Muhammad Iqbal <ammaar.iqbal@gmail.com>
2018-02-18 15:59:36 -0500

    nice test case

--- test.c
+++ test.c
@@ -7,6 +7,11 @@ void test(int signum) {
         int l = (((i+1) *32)/65)*(((i+1) *32)/65);
         // printf("%d ", l);
     }
+    printf("in test %d, %d\n", signum, 0);
+    for (i = 0; i < 999999; i++) {
+        int l = (((i+1) *32)/65)*(((i+1) *32)/65);
+        // printf("%d ", l);
+    }
 	printf("in test %d, %d\n", signum, 1);
     for (i = 0; i < 999999; i++) {
         int l = (((i+1) *32)/65)*(((i+1) *32)/65);
@@ -43,35 +48,37 @@ void test(int signum) {
         // printf("%d ", l);
     }
     printf("in test %d, %d\n", signum, 8);
-    for (i = 0; i < 999999; i++) {
-        int l = (((i+1) *32)/65)*(((i+1) *32)/65);
-        // printf("%d ", l);
-    }
-    printf("in test %d, %d\n", signum, 9);
 
-    my_pthread_exit(NULL);
+    char * testString = malloc(10);
+    sprintf(testString, "test %d", signum);
+
+    my_pthread_exit(testString);
 }
 
 // Temporary main for testing.
 int main(int argc, char ** argv) {
 
-    my_pthread_t thread;
+    my_pthread_t thread[5];
 
-	my_pthread_create(&thread, NULL, test, 1);
+	my_pthread_create(&(thread[0]), NULL, test, 0);
     printf("1\n");
-    my_pthread_create(&thread, NULL, test, 2);
+    my_pthread_create(&(thread[1]), NULL, test, 1);
     printf("2\n");
-    my_pthread_create(&thread, NULL, test, 3);
+    my_pthread_create(&(thread[2]), NULL, test, 2);
     printf("3\n");
-    my_pthread_create(&thread, NULL, test, 4);
+    my_pthread_create(&(thread[3]), NULL, test, 3);
     printf("4\n");
-    my_pthread_create(&thread, NULL, test, 5);
+    my_pthread_create(&(thread[4]), NULL, test, 4);
 
 	printf("back in main\n");
 
-    my_pthread_join(thread, NULL);
+    char * string[5];
+    int i;
 
-    printf("joined!\n");
-
+    for (i = 0; i < 5; i++) {
+        my_pthread_join(thread[i], &(string[i]));
+        printf("joined! %s\n", string[i]);
+    }
+    
 	return 0;
 }
