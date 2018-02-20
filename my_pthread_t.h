8ccbd749439e89eb1dfb685e602930edf621d058
Ammaar Muhammad Iqbal <ammaar.iqbal@gmail.com>
2018-02-19 23:21:12 -0500

    added macros

--- my_pthread_t.h
+++ my_pthread_t.h
@@ -90,3 +90,17 @@ int my_pthread_mutex_unlock(my_pthread_mutex_t *mutex);
 int my_pthread_mutex_destroy(my_pthread_mutex_t *mutex);
 
 #endif
+
+#define USE_MY_PTHREAD 1 (comment it if you want to use real pthread)
+
+#ifdef USE_MY_PTHREAD
+#define pthread_t my_pthread_t
+#define pthread_mutex_t my_pthread_mutex_t
+#define pthread_create my_pthread_create
+#define pthread_exit my_pthread_exit
+#define pthread_join my_pthread_join
+#define pthread_mutex_init my_pthread_mutex_init
+#define pthread_mutex_lock my_pthread_mutex_lock
+#define pthread_mutex_unlock my_pthread_mutex_unlock
+#define pthread_mutex_destroy my_pthread_mutex_destroy
+#endif
